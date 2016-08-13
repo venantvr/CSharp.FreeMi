3d2a4fd47e3c375e9be8ca5f2f5c77505cf1f102
Rénald <Rnald@RVV-8>
2016-08-14 00:05:51 +0200

    Add project files.

---
+++ FreeMi.Console/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using FreeMi.Core;
+
+namespace FreeMi.Console
+{
+    internal class Program
+    {
+        private static Device Device { get; set; }
+
+        private static void Main(string[] args)
+        {
+            Device = new Device();
+            Device.Start();
+            try
+            {
+                var arr = new WaitHandle[1];
+                arr[0] = new ManualResetEvent(false);
+
+                while (false == WaitHandle.WaitAll(arr, new TimeSpan(0, 0, 10)))
+                {
+                }
+
+                //System.Console.WriteLine(Resources.PressEnterToQuit);
+                //System.Console.ReadLine();
+            }
+            finally
+            {
+                if (Device != null)
+                {
+                    Device.Stop();
+                    Device = null;
+                }
+            }
+        }
+    }
+}
---
+++ FreeMi.Core/Entries/IContainer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FreeMi.Core.Entries
+{
+    internal interface IContainer
+    {
+        Entry GetContent(bool isFreeboxV5);
+    }
+}
